ce82c92f083c3bacf18b26a58e81b1ddabaf6cfe
amores <amores.dev@outlook.com>
2026-03-16 16:49:25 -0600

    feat: Add separators between sections on the Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Contact from "@/components/sections/Contact"
 import Hero from "@/components/sections/Hero"
 import Projects from "@/components/sections/Projects"
 import Skills from "@/components/sections/Skills"
+import { Separator } from '@/components/ui/separator'
 
 const Home = () => {
   return (
@@ -12,9 +13,13 @@ const Home = () => {
       <Navbar />
       <main>
         <Hero />
+        <Separator className="bg-zinc-800" />
         <About />
+        <Separator className="bg-zinc-800" />
         <Skills />
+        <Separator className="bg-zinc-800" />
         <Projects />
+        <Separator className="bg-zinc-800" />
         <Contact />
       </main>
       <Footer />
